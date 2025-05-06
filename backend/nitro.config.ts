38a030a417994ddedd133259ff190aaf19f9d145
Jonathan Aldas <jonaaldas@gmail.com>
2025-05-06 20:52:52 +0200

    init

---
+++ backend/nitro.config.ts
@@ -0,0 +1,6 @@
+//https://nitro.unjs.io/config
+import '../env'
+export default defineNitroConfig({
+  srcDir: 'server',
+  compatibilityDate: '2025-05-06',
+})
---
+++ env.ts
@@ -0,0 +1,32 @@
+import * as z from 'zod'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.resolve(process.cwd(), '../.env') })
+
+export const backendEnv = z.object({
+  DATABASE_URL: z.string(),
+})
+
+const env = {
+  backend: null,
+}
+
+try {
+  const backendEnvSchema = backendEnv.parse(process.env)
+  env.backend = backendEnvSchema
+} catch (error) {
+  if (error instanceof z.ZodError) {
+    let message = 'Missing required values in .env:\n'
+    error.issues.forEach(issue => {
+      message += `${issue.path[0]}\n`
+    })
+    const e = new Error(message)
+    e.stack = ''
+    throw e
+  } else {
+    console.error(error)
+  }
+}
+
+export default env
